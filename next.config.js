c9054b93b8163681d8e8b9293e3cbbe11ef7771e
suchirnaik <122578575+suchirnaik@users.noreply.github.com>
2024-04-26 08:25:54 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		domains: ["img.icons8.com","v5.airtableusercontent.com"],
-	},
-};
-
-module.exports = nextConfig;
