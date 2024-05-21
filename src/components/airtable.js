c75859c5eaceadeafac4093c9edf12ec602af505
Sumanth <sumanthmylar@sumanths-air.notrust.clients.wireless.pfw.edu>
2024-05-21 13:49:03 -0400

    Vercel deployment fix, removed apiKey & base ID from airtable.js file which was necessary for dev

--- src/components/airtable.js
+++ src/components/airtable.js
@@ -1,7 +1,7 @@
 import Airtable from 'airtable';
 
 const base = new Airtable({
-  apiKey: 'patW2qRkI1xtyUBCd.0b088b7cfffdbe16992df7f8b411d5d65e9036c1ac4563be91df8f36e62bf47a',
-}).base('appPmVLzr3dxbzPry');
+  apiKey: 'not in use',
+}).base('not in use');
 
 export default base;
